e1efe744d28a9581130202c1914dd7916de6b80a
saviel <savman1994@gmail.com>
2026-03-14 09:54:03 +0100

    Quartz sync: Mar 14, 2026, 9:54 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Books",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
